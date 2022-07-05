dff2d948c82ea5f1fec3a78d1fe72980dcc1ab6c
kanno <812137533@qq.com>
2022-07-05 17:29:13 +0800

    feat(scale): complete with-scale

--- packages/vue/src/utils/create/with-scale-install.ts
+++ packages/vue/src/utils/create/with-scale-install.ts
@@ -1,13 +1,20 @@
 import { h, defineComponent, provide, inject, reactive } from 'vue'
-import { omit, assign } from '../format'
+import { omit, assign, isUndefined, isNumber } from '../format'
 import type { App, ExtractPropTypes, InjectionKey } from 'vue'
+
+/**
+ * Inspired by scala from geist-core
+ * I want to keep the API style as consistent
+ * as possible. Alough the implementation is
+ * different.
+ */
 
 export type WithScaleInstall<T> = T & {
   install(app: App): void
   name: string
 }
 
-export const scaleProps = {
+const scaleProps = {
   scale: {
     type: Number,
     default: 1
@@ -108,12 +115,34 @@ export const scaleProps = {
 
 export type ScaleProps = ExtractPropTypes<typeof scaleProps>
 
+export type ScaleInputs =
+  | 'pt'
+  | 'pr'
+  | 'pb'
+  | 'pl'
+  | 'px'
+  | 'py'
+  | 'mt'
+  | 'mr'
+  | 'mb'
+  | 'ml'
+  | 'mx'
+  | 'my'
+  | 'width'
+  | 'height'
+  | 'font'
+
+export type ModifersPipe = (baseScale: number, defaultValue?: string | number) => string
+
 interface ScaleContext {
-  props: ScaleProps
-  //   unit: string
-  //   SCALES: {
-  //     pt:
-  //   }
+  SCALES: Record<ScaleInputs, ModifersPipe>
+  getAllScaleProps: () => ScaleProps
+}
+
+const reduceScaleCoefficient = (scale: number) => {
+  if (scale === 1) return scale
+  const diff = Math.abs((scale - 1) / 2)
+  return scale > 1 ? 1 + diff : 1 - diff
 }
 
 const READONLY_SCLAE_KEY: InjectionKey<ScaleContext> = Symbol('ScaleRenderKey')
@@ -131,13 +160,42 @@ export const withScaleInstaller = <T>(component: T) => {
     inheritAttrs: false,
     props: scaleProps,
     setup(props) {
-      //   translate scale system.
-      const scaleTranslate = () => {
-        //
-      }
+      const modifers =
+        (attrValue: string | number | undefined): ModifersPipe =>
+        (baseScale, defaultValue) => {
+          if (!baseScale) {
+            baseScale = 1
+            defaultValue = defaultValue || 0
+          }
+          const stand = reduceScaleCoefficient(props.scale) * baseScale
+          if (isUndefined(attrValue)) {
+            if (!isUndefined(defaultValue)) return `${defaultValue}`
+            return `calc(${stand} * ${props.unit})`
+          }
+          if (!isNumber(attrValue)) return `${attrValue}`
+          const userStand = stand * Number(attrValue)
+          return `calc(${userStand} * ${props.unit})`
+        }
 
       createScaleContext({
-        props
+        SCALES: {
+          pt: modifers(props.paddingTop ?? props.pt ?? props.py ?? props.padding),
+          pr: modifers(props.paddingRight ?? props.pr ?? props.px ?? props.padding),
+          pb: modifers(props.paddingBottom ?? props.pb ?? props.py ?? props.padding),
+          pl: modifers(props.paddingLeft ?? props.pl ?? props.px ?? props.padding),
+          px: modifers(props.px ?? props.paddingLeft ?? props.paddingRight ?? props.pl ?? props.pr ?? props.padding),
+          py: modifers(props.py ?? props.paddingTop ?? props.paddingBottom ?? props.pt ?? props.pb ?? props.padding),
+          mt: modifers(props.marginTop ?? props.mt ?? props.my ?? props.margin),
+          mr: modifers(props.marginRight ?? props.mr ?? props.mx ?? props.margin),
+          mb: modifers(props.marginBottom ?? props.mb ?? props.my ?? props.margin),
+          ml: modifers(props.marginLeft ?? props.ml ?? props.mx ?? props.margin),
+          mx: modifers(props.mx ?? props.marginLeft ?? props.marginRight ?? props.ml ?? props.mr ?? props.margin),
+          my: modifers(props.my ?? props.marginTop ?? props.marginBottom ?? props.mt ?? props.mb ?? props.margin),
+          width: modifers(props.paddingRight ?? props.pr ?? props.px ?? props.padding),
+          height: modifers(props.width ?? props.w),
+          font: modifers(props.font)
+        },
+        getAllScaleProps: () => props
       })
     },
     render() {
